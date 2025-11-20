f7008b0d71d5d04dbde130af1cd036a61b7c3947
Kieu Thanh Nghi <souleater2222001@gmail.com>
2025-11-20 17:12:47 +0700

    ver11

---
+++ Assets/_Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+
+    public void LoadScene() => SceneManager.LoadScene(sceneName);
+}
